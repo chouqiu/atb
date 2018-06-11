2c3e5142bf3db50fc8ef16e5ea005839f51eb040
chouqiu <5884597@qq.com>
2018-06-12 00:14:57 +0800

    atb 2.0

--- avtb.py
+++ avtb.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import re
 import os
 import socket
 import threading
@@ -19,6 +20,8 @@ socket.setdefaulttimeout(60)
 info_lock = threading.Lock()
 info_arr = []
 download_count = 0
+video_arr = {}
+main_host="http://www.999avtb.com/"
 
 class MyExcept(Exception):
     pass
@@ -69,7 +72,7 @@ def fetch_link(url, idx):
 
         f = open(file_name, 'wb')
         f.seek(file_size_dl)
-        block_sz = 4 * 1024 * 1024
+        block_sz = 256 * 1024
         cnt = 0
         while True:
             try:
@@ -215,51 +218,51 @@ def down_file(url, id):
         raise MyExcept("down fail: %d" % (fail))
 
 
-def fetch_url(args):
+# type=0: list, type=1: download
+def fetch_url(arg, arg_type):
     global info_lock
     global info_arr
     global download_count
+    global video_arr
     # 网址
     # Accept - Language: zh - CN
     # Connection: Keep - Alive
     # Cookie: JSESSIONID=76782BCA557E307FBC7F29CB08E250FF;tk=VDAxKt94hbSfakQbTHXBgDSCDexK3E0EK7VJsIrwE7Mko1210;route=9036359bb8a8a461c164a04f8f50b252;BIGipServerotn=1290797578.38945.0000;BIGipServerpool_passport=300745226.50215.0000;current_captcha_type=Z;_jc_save_fromStation=%u5E7F%u5DDE%2CGZQ;_jc_save_toStation=%u6DF1%u5733%2CSZQ;_jc_save_fromDate=2017-10-07;_jc_save_toDate=2017-10-03;_jc_save_wfdc_flag=dc
-    print("get args: %s" % (args,))
-
-    url = args
-    h = ""
-    try:
-        h = url.split('/')[2]
-    except Exception as e:
-        print("url %s invalid." % (url,))
-        return
+    print("get args: %s" % (arg,))
+    url = arg
 
     info = {}
     info["url"] = url
-    info["host"] = h
     info["stat"] = 0
     info["file"] = "NULL"
     info["file_size"] = -1
     info["file_dl"] = 0
-    info_lock.acquire()
-    info["id"] = len(info_arr)
-    info_arr.append(info)
-    info_lock.release()
 
-    print("fetch host: %s" % (h))
-    # url = "http://www.avtb004.com/4048/"
-    headers = {
-        'User-Agent': 'Mozilla/5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063',
-        'Host': h}
-    # 'Accept':'text/html, application/xhtml+xml, image/jxr, */*',
-    # 'Accept-Encoding':'gzip, deflate, br',
-    # 'Cookie':'JSESSIONID=76782BCA557E307FBC7F29CB08E250FF;tk=VDAxKt94hbSfakQbTHXBgDSCDexK3E0EK7VJsIrwE7Mko1210;route=9036359bb8a8a461c164a04f8f50b252;BIGipServerotn=1290797578.38945.0000;BIGipServerpool_passport=300745226.50215.0000;current_captcha_type=Z;_jc_save_fromStation=%u5E7F%u5DDE%2CGZQ;_jc_save_toStation=%u6DF1%u5733%2CSZQ;_jc_save_fromDate=2017-10-07;_jc_save_toDate=2017-10-03;_jc_save_wfdc_flag=dc'}
+    if arg_type == 1:
+        info_lock.acquire()
+        info["id"] = len(info_arr)
+        info_arr.append(info)
+        info_lock.release()
 
-    # 请求
-    request = Request(url=url, headers=headers)
-
-    # 爬取结果
-    down_ok = 1
+    h = ""
     try:
+        h = url.split('/')[2]
+        info["host"] = h
+
+        print("fetch host: %s" % (h))
+        # url = "http://www.avtb004.com/4048/"
+        headers = {
+            'User-Agent': 'Mozilla/5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063',
+            'Host': h}
+        # 'Accept':'text/html, application/xhtml+xml, image/jxr, */*',
+        # 'Accept-Encoding':'gzip, deflate, br',
+        # 'Cookie':'JSESSIONID=76782BCA557E307FBC7F29CB08E250FF;tk=VDAxKt94hbSfakQbTHXBgDSCDexK3E0EK7VJsIrwE7Mko1210;route=9036359bb8a8a461c164a04f8f50b252;BIGipServerotn=1290797578.38945.0000;BIGipServerpool_passport=300745226.50215.0000;current_captcha_type=Z;_jc_save_fromStation=%u5E7F%u5DDE%2CGZQ;_jc_save_toStation=%u6DF1%u5733%2CSZQ;_jc_save_fromDate=2017-10-07;_jc_save_toDate=2017-10-03;_jc_save_wfdc_flag=dc'}
+
+        # 请求
+        request = Request(url=url, headers=headers)
+
+        # 爬取结果
+        down_ok = 1
         http_ok = False
         retry = 1
         while not http_ok and retry <= 10:
@@ -283,11 +286,33 @@ def fetch_url(args):
 
         # 打印结果
         soup = BeautifulSoup(data, "lxml")
+        found = 0
+
+        # 抓取视频链接
         for child in soup.find_all("source", label="360p"):
+            found = found + 1
             print(child["src"])
             downrst = fetch_link(child["src"], info["id"])
             print("get link %s, %d" % (downrst[0], downrst[1]))
             break
+
+        found_list = 0
+        print("")
+        for child in soup.find_all("a", class_="thumbnail") :
+            vinfo = child['href'].split('/')
+            #if len(vinfo) < 3 :
+            #    break
+            found_list = found_list + 1
+            video_arr.update({vinfo[1]:{'name':vinfo[2]}})
+            for cc in child.find_all("span", class_="video-rating") :
+                video_arr[vinfo[1]].update({'rate':cc.get_text().strip()})
+                break
+            print("%s [%s]  %s" % (vinfo[1], video_arr[vinfo[1]]['rate'], video_arr[vinfo[1]]['name']))
+        
+        if found <= 0 and found_list <= 0 :
+            print("no resource found for url %s." % (url))
+            down_ok = 0
+
 
             # 打印爬取网页的各类信息
             # print(type(response))
@@ -299,9 +324,9 @@ def fetch_url(args):
         print("get url %s fail: %s" % (url, e))
     except Exception as e:
         down_ok = 0
-        print("get url %s fail." % (url,))
+        print("get url %s fail2: %s" % (url, e))
     finally:
-        if down_ok <= 0:
+        if down_ok <= 0 and arg_type == 1 :
             info_lock.acquire()
             info_arr[info["id"]]["stat"] = -2
             fn = info_arr[info["id"]]["file"]
@@ -326,19 +351,32 @@ if __name__ == "__main__":
         if user_input == "exit" or user_input == "quit":
             print("current threads %d, exiting ..." % (download_count))
             break
-        if len(user_input)>4:
+        if len(user_input) > 2 :
             # print(user_input)
             if download_count >= 10:
                 print("current %d threads running ,please wait..." % (download_count))
                 continue
             else:
-                info_lock.acquire()
-                download_count = download_count+1
-                info_lock.release()
-                t = threading.Thread(target=fetch_url, args=(user_input,))
-                t.setDaemon(True)
-                # threads[t.getName()] = 1
-                t.start()
+                uu = user_input
+                if re.match(r"^[0-9]+$", user_input) :
+                    uu = main_host + user_input
+                if re.match(r"^http", uu) :
+                    info_lock.acquire()
+                    download_count = download_count+1
+                    info_lock.release()
+                    t = threading.Thread(target=fetch_url, args=(uu, 1))
+                    t.setDaemon(True)
+                    # threads[t.getName()] = 1
+                    t.start()
+        if re.match(r"^list$", user_input) :
+            print("fetch list ...")
+            video_arr = {}
+            info_lock.acquire()
+            download_count = download_count+1
+            info_lock.release()
+            t = threading.Thread(target=fetch_url, args=(main_host, 0))
+            t.setDaemon(True)
+            t.start()
 
         info_lock.acquire()
         for info in info_arr:
